c0af8d2c3af16653e9fde66ce8dba1497c9e5f81
juanbz12 <juanbazurto801@gmail.com>
2026-03-04 19:44:23 +0100

    Update static.ts

--- Dashboard-Creator/server/static.ts
+++ Dashboard-Creator/server/static.ts
@@ -1,5 +1,10 @@
+import express, { type Express } from "express";
+import fs from "fs";
+import path from "path";
+
 export function serveStatic(app: Express) {
- const distPath = path.resolve(__dirname, "../public");
+  const distPath = path.join(process.cwd(), "dist/public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -8,8 +13,7 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // SPA fallback
-  app.get("*", (_req, res) => {
+  app.use("*", (_req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
